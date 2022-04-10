8e30ffce797106290858e45a7e8ce47850ebb2c0
Artur Racinowski <artur.racinowski@gmail.com>
2022-04-10 19:58:29 +0200

    Porządki w plikach.

---
+++ experiments.hs
@@ -0,0 +1,5 @@
+import Control.Monad.Writer
+
+dzielniki :: Int -> [Int]
+dzielniki n = [ x | x <- [1..n-1], mod n x == 0]:q
+
--- utilities.hs
+++ utilities.hs
@@ -1,9 +1,9 @@
-module Utilities (isPrime,isqrt) where
+module Utilities (isPrime,intSqrt) where
 
 isPrime :: Int -> Bool 
 isPrime n 
     | n < 2 = False   
-    | otherwise = not $ any (\x -> mod n x == 0) [2.. isqrt n]
+    | otherwise = not $ any (\x -> mod n x == 0) [2,3..intSqrt n]
 
-isqrt :: Int -> Int
-isqrt = floor . sqrt . fromIntegral
+intSqrt :: Int -> Int
+intSqrt = floor . sqrt . fromIntegral
