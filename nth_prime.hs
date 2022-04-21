a7cadce8da3f9502aea433cac44bb0c46f27e985
ArtRac <artur.racinowski@gmail.com>
2022-04-21 09:57:44 +0200

    Update nth_prime.hs

--- nth_prime.hs
+++ nth_prime.hs
@@ -4,9 +4,24 @@
 -- we can see that the 6th prime is 13.
 -- If your language provides methods in the standard library to deal with prime numbers, 
 -- pretend they don't exist and implement them yourself.
-import Utilities
+
+module Nth_prime (nth) where
 
 nth :: Int -> Maybe Integer
 nth n 
-    | n > 0 = Nothing  
-    | otherwise = Nothing 
+    | n < 1 = Nothing  
+    | otherwise = Just (nth' n 1 2)
+
+nth' :: Int  -> Int  -> Int  -> Integer
+nth' x y z
+    | x == y && isPrime z = toInteger z
+    | y < z && isPrime z = nth' x (y+1) (z+1)
+    | not $ isPrime z = nth' x y (z+1)
+
+isPrime :: Int -> Bool 
+isPrime n 
+    | n < 2 = False   
+    | otherwise = not $ any (\x -> mod n x == 0) [2,3..intSqrt n]
+
+intSqrt :: Int -> Int
+intSqrt = floor . sqrt . fromIntegral
