8cacd1681caaadb5b62bd175e8571804370d83bb
ArtRac <artur.racinowski@gmail.com>
2022-04-21 14:45:26 +0200

    Update pe_145.hs

--- pe_145.hs
+++ pe_145.hs
@@ -1,1 +1,15 @@
 -- https://projecteuler.net/problem=145
+module pe_145 ( ... )
+
+import Test.HUnit
+
+test1 :: Test
+test1 = TestCase $ assertEqual "Opis testu1." ({-Oczekiwana wartość-}) ({-Wyliczona wartość-})
+
+test2 :: Test
+test2 = TestCase $ assertEqual "Opis testu2." ({-Oczekiwana wartość-}) ({-Wyliczona wartość-})
+
+test :: IO Counts
+test = runTestTT $ TestList [test1, test1]
+
+main = error "You need to implement this function"
